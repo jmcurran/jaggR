1ebc41de873b9719d22f662bfd92ac7d23c8fc01
James Curran <j.curran@auckland.ac.nz>
2019-06-12 21:21:53 +1200

    Getting the datasets under control

---
+++ R/datasets.R
@@ -0,0 +1,39 @@
+#' Energy and fat in chocolate bars
+#' 
+#' The amount of fat (g) and energy (Cal) in 16 chocolate bars.
+#' Source is unknown, but we would be happy to give credit if 
+#' someone tells us.
+#' 
+#' 
+#' @format A data.frame with 16 rows and 2columns: 
+#' \describe{
+#' \item{energy}{energy, in Calories = kilocalories}
+#' \item{fat}{fat content, in grams}
+#' }
+#' @source Source is unknown, but we would be happy to give credit if 
+#' someone tells us.
+#' @keywords datasets
+"chocolate.df"
+
+
+#' Impact strength of insulation cuts in foot-pounds.
+#'	
+#' @format a data.frame with 100 rows and 3 colums:
+#' \describe{
+#'	\item{Lot}{Lot of insulating material}
+#'	\item{Cut}{Lengthwise (Length) or crosswise (Cross)}
+#'	\item{Strength}{Impact strength, in foot-pounds (ft-lb)}
+#' }	
+#'@source Ostle, B. (1963). Statistics in Research: Basic Concepts and Techniques for Research. 
+#'Ames, Iowa. Iowa State University Press.
+"insulation.df"
+
+#' Hedgehog growth
+#' 
+#' @format a data.frame with 77 rows and 2 columns:
+#' \describe{
+#' \item{date}{Date in DD-Month-YYYY format}
+#' \item{weight}{weight of the hedgehog, in grams}
+#' }
+#' @source David Lucy
+"hedgehog-growth.df"
---
+++ R/jaggR-package.R
@@ -0,0 +1,12 @@
+#' jaggR: Supporting files and functions for the book Bayesian Modelling with JAGS
+#' 
+#' A set of functions used in teaching STATS 201/208 Data Analysis at the University of Auckland. The functions are designed to make parts of R more accessible to a large undergraduate population who are mostly not statistics majors.
+#' @docType package
+#' @name jaggR
+#' 
+#' @import stats
+#' @import graphics 
+#' @importFrom utils packageDescription
+#' @importFrom utils read.table
+#' @author James Curran, David Lucy
+NULL

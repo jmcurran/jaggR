8c7e0bab89392c967704fc85d67fbf452924ac87
jmcurran <james.m.curran@gmail.com>
2023-11-01 14:34:15 -0500

    Documented print.summary.mcmc

--- R/print.summary.mcmc.R
+++ R/print.summary.mcmc.R
@@ -1,3 +1,20 @@
+#' S3 print method for objects of type summary.mcmc
+#'
+#' This function overrides the hidden method in the \code{coda} package
+#' that provides a print method for the output of the \code{\link{coda}{summary}}
+#' function. The idea is to be able to suppress some of the output so that
+#' only the summary statistics of interest are shown. This is primarily used in the
+#' preparation of the book.
+#'
+#' @param x an object of type \code{summary.mcmc}.
+#' @param digits The number of digits to print.
+#' @param runDetails if \code{TRUE} print the details of the sampling.
+#' @param means if \code{TRUE} print the posterior means.
+#' @param quantiles if \code{TRUE} print the posterior quantiles.
+#' @param ... other arguments passed to \code{print}.
+#'
+#' @return x is invisibly returned
+#' @export
 print.summary.mcmc = function (x, digits = max(3, .Options$digits - 3),
                                runDetails = FALSE,
                                means = FALSE,
